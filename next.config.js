76886561fddef0b67d751cfeb80ebe62baad3e84
yawei.li <phxaz010@gmail.com>
2023-12-20 20:54:36 +0800

    chore(config): add output

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withTM = require('next-transpile-modules')(['react-syntax-highlighter']);
 
 const nextConfig = withPlugins([withTM, withFonts], {
   reactStrictMode: false,
+  output: "export",
   images: {
     unoptimized: true
   }
